598fddcafd251819e8f3b7927ffdf7b21db479c6
Marina-Labinskaya <life.joy.spring@gmail.com>
2022-11-10 13:41:37 +0300

    added periodical boundaries for particle

---
+++ src/Modules/Particle/include/ParticleSolver.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "Dimension.h"
+#include "Grid.h"
+#include "Particle.h"
+#include "Species.h"
+
+namespace pfc {
+    class ParticleSolver {
+    public:
+        template <class T_ParticleArray, GridTypes gridType>
+        void updateParticlePosition(Grid<FP, gridType> * grid, T_ParticleArray* particleArray) {};
+
+    };
+
+    
+    class PeriodicalParticleSolver : ParticleSolver {
+    public:
+        template <class T_ParticleArray, GridTypes gridType>
+        void updateParticlePosition(Grid<FP, gridType> * grid, T_ParticleArray* particleArray);
+    };
+
+    template<class T_ParticleArray, GridTypes gridType>
+    void PeriodicalParticleSolver::updateParticlePosition(Grid<FP, gridType> * grid, T_ParticleArray* particleArray)
+    {
+        typedef typename T_ParticleArray::ParticleProxyType ParticleProxyType;
+        typedef typename VectorTypeHelper<Dimension::Three, Real>::Type PositionType;
+        for (int i = 0; i < particleArray->size(); i++)
+        {
+            ParticleProxyType particle = (*particleArray)[i];
+            PositionType particlePosition = particle.getPosition();
+            FP3 minCoords = grid->origin + grid->steps * grid->getNumExternalLeftCells();
+            FP3 maxCoords = minCoords + grid->numInternalCells * grid->steps;
+
+            if (particlePosition.x < minCoords.x)
+                particlePosition.x = particlePosition.x + (grid->numInternalCells.x * grid->steps.x);
+            if (particlePosition.x > maxCoords.x)
+                particlePosition.x = particlePosition.x - (grid->numInternalCells.x * grid->steps.x);
+
+            if (particlePosition.y < minCoords.y)
+                particlePosition.y = particlePosition.y + (grid->numInternalCells.y * grid->steps.y);
+            if (particlePosition.y > maxCoords.y)
+                particlePosition.y = particlePosition.y - (grid->numInternalCells.y * grid->steps.y);
+
+            if (particlePosition.z < minCoords.z)
+                particlePosition.z = particlePosition.z + (grid->numInternalCells.z * grid->steps.z);
+            if (particlePosition.z > maxCoords.z)
+                particlePosition.z = particlePosition.z - (grid->numInternalCells.z * grid->steps.z);
+
+            particle.setPosition(particlePosition);
+        }
+    }
+}
